ee3c985dd7020bdf9c7f82b0c006d06a48656bfd
Mamadou Mouctar DIALLO <mamadoumouctardiallo@esp.sn>
2021-03-05 21:47:29 +0000

    fix base html base.html.twig and home, profile controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    /**
+     * @Route("/dashboar", name="dash.index", methods="GET")
+     */
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/test", methods="GET")
+     */
+    public function test(): Response
+    {
+        return $this->render('home/tesst.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
